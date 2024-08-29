9bacbbbab80ac1ff2c5f5ccb671de7cb612effb2
jayasundarrajan <jaya.sundarrajan20@gmail.com>
2024-08-29 20:49:32 +0000

    Fixed bugs in buggy.cpp and passed all tests

--- LE1/buggy.cpp
+++ LE1/buggy.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstring> // for memcpy
+using namespace std;
 
 struct Point {
     int x, y;
@@ -10,33 +12,65 @@ struct Point {
 class Shape {
     int vertices;
     Point** points;
+public: 
 
     Shape (int _vertices) {
         vertices = _vertices;
         points = new Point*[vertices+1];
+        // allocating memory for points
+        for (int i = 0; i < vertices; i++) {
+            points[i] = new Point();
+        }
     }
 
     ~Shape () {
+        // deallocate memory for points
+        for (int i = 0; i < vertices; i++) {
+            delete points[i];
+        }
+        delete[] points;
+       
     }
 
-    void addPoints (/* formal parameter for unsized array called pts */) {
-        for (int i = 0; i <= vertices; i++) {
-            memcpy(points[i], &pts[i%vertices], sizeof(Point));
+    void addPoints (Point* pts) {
+        for (int i = 0; i < vertices; i++) {
+            //memcpy(points[i], &pts[i%vertices], sizeof(Point));
+            *points[i] = pts[i % vertices]; // copy the point data
         }
     }
 
-    double* area () {
+    // double* area () {
+    //     int temp = 0;
+    //     for (int i = 0; i < vertices; i++) {
+    //         // FIXME: there are two methods to access members of pointers
+    //         //        use one to fix lhs and the other to fix rhs
+    //         int lhs = points[i] -> x * points[(i+1) % vertices] -> y; 
+    //         int rhs = (*points[(i + 1) % vertices]).x * (*points[i]).y;
+    //         temp += (lhs - rhs);
+    //     }
+    //     double area = abs(temp)/2.0;
+    //     return &area; // returns local pointer not good
+
+    // }
+
+    double area () {
         int temp = 0;
-        for (int i = 0; i <= vertices; i++) {
+        for (int i = 0; i < vertices; i++) {
             // FIXME: there are two methods to access members of pointers
             //        use one to fix lhs and the other to fix rhs
-            int lhs = points[i].x * points[i+1].y;
-            int rhs = points[i+1].x * points[i].y;
+            int lhs = points[i] -> x * points[(i+1) % vertices] -> y; 
+            int rhs = (*points[(i + 1) % vertices]).x * (*points[i]).y;
             temp += (lhs - rhs);
         }
         double area = abs(temp)/2.0;
-        return &area;
+        return area;
+        
     }
+
+
+
+
+
 };
 
 int main () {
@@ -46,10 +80,14 @@ int main () {
     //          tri2 = (1, 2)
     //          tri3 = (2, 0)
 
+    Point tri1(0, 0);
+    Point tri2(1, 2);
+    Point tri3(2, 0);
+
     // adding points to tri
     Point triPts[3] = {tri1, tri2, tri3};
     Shape* tri = new Shape(3);
-    tri.addPoints(triPts);
+    tri -> addPoints(triPts);
 
     // FIXME: create the following points using your preferred struct
     //        definition:
@@ -58,10 +96,26 @@ int main () {
     //          quad3 = (2, 2)
     //          quad4 = (2, 0)
 
+    Point quad1(0, 0);
+    Point quad2(0, 2);
+    Point quad3(2, 2);
+    Point quad4(2, 0);
+
     // adding points to quad
     Point quadPts[4] = {quad1, quad2, quad3, quad4};
     Shape* quad = new Shape(4);
-    quad.addPoints(quadPts);
+    quad -> addPoints(quadPts);
 
     // FIXME: print out area of tri and area of quad
+
+    cout << "Area of tri: " << tri -> area() << endl;
+    cout << "Area of quad: " << quad -> area() << endl;
+
+// need to deallocate memory
+    delete tri;
+    delete quad;
+
+    return 0;
+
+
 }
